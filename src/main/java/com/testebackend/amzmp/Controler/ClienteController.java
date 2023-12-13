2807735726ac9247c9973a864c6e3a575a0995f6
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2023-12-12 21:55:37 -0300

    Crieação dos metodos de cadastro

--- src/main/java/com/testebackend/amzmp/Controler/ClienteController.java
+++ src/main/java/com/testebackend/amzmp/Controler/ClienteController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties.RSocket.Client;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.testebackend.amzmp.Model.Cliente;
@@ -15,6 +17,9 @@ import com.testebackend.amzmp.Repository.ClienteRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -31,10 +36,34 @@ public class ClienteController {
         this.repository = repository;
     }
 
+    //Carregar a listagem clientes
+
     @GetMapping("/listar-cliente")
-    public String listarCliente(Model model ) {
+    public String ListarCliente(Model model ) {
         model.addAttribute("cliente" , repository.findAll());
         return "cliente";
+    }
+
+    //Carregar o formulario cadastro
+    
+    @GetMapping("/novo-cliente")
+    public String CadastrarCliente(Cliente cliente, Model model) {
+        model.addAttribute("cliente", cliente);
+        return "novo-cliente";
+    }
+    
+    //Cadastrar (inserir)
+
+    @PostMapping("/add-cliente")
+    public String addClinte(Cliente cliente, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+                return "/novo-cliente";
+        }
+
+        Cliente clienteDb = repository.save(cliente);
+        cliente.setStatus_cliente(true);
+
+        return "redirect:/amz-mp/cliente/listar-cliente";
     }
     
 
