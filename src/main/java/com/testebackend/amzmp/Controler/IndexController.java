440caa4ce223dabbff546783f5e08e3295e9ceb1
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2023-12-13 15:58:39 -0300

    indexController

--- src/main/java/com/testebackend/amzmp/Controler/IndexController.java
+++ src/main/java/com/testebackend/amzmp/Controler/IndexController.java
@@ -2,9 +2,33 @@ package com.testebackend.amzmp.Controler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/amz-mp/ap1")
 public class IndexController {
+    @GetMapping("/index")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/painel")
+    public String painel() {
+        return "/amz-mp/cliente/listar-cliente";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastro() {
+        return "/amz-mp/cliente/novo-cliente";
+    }
+
+     @GetMapping("/editar")
+    public String editar() {
+        return "/amz-mp/cliente/editar";
+    }
+
+    
     
 }
